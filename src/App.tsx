63424ff67cac5e0416193faa55d67b1f84784d24
Anto Gibson <anto.sf3688@gmail.com>
2025-02-26 18:05:31 +0530

    Some changes for Animation Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Components/HomePage';
+import AnimationComponent from './Components/AnimatioComponent/AnimationComponent';
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <AnimationComponent/>
     </div>
   );
 }
--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,42 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import "./HomePage.css";
-import {
-  AxesDirective,
-  AxisDirective,
-  CircularGaugeComponent,
-  PointersDirective,
-  PointerDirective,
-  FontModel,
-  GaugeTheme,
-  IThemeStyle,
-} from "@syncfusion/ej2-react-circulargauge";
 
 const HomePage = () => {
-  const fontModel: FontModel = {
-    size: "18",
-    color: "Red",
-    fontWeight: "400",
-    fontStyle: "Underline",
-    fontFamily: "Cambria",
-  };
-  const componentRef = useRef<CircularGaugeComponent>(null);
-
-  const gaugeTheme : GaugeTheme = 'Material3';
-  const themeStyle : IThemeStyle = {
-      backgroundColor: 'red',
-      titleFontColor: '#ffffff',
-      tooltipFillColor: 'black',
-      tooltipFontColor: "#ffffff",
-      tooltipFontSize: "14",
-      lineColor: "black",
-      labelColor: "#ffffff",
-      majorTickColor: "red",
-      minorTickColor: "white",
-      pointerColor: "white",
-      needleColor: "black",
-      needleTailColor: "red",
-      capColor: "white"
-  } 
+  const url : string = '';
 
   function removePopup() {
     const matchingDivs = Array.from(document.querySelectorAll("div")).filter(
@@ -49,27 +15,39 @@ const HomePage = () => {
     matchingDivs[0]?.parentElement?.removeChild(matchingDivs[0]);
   };
 
-  useEffect(()=>{
-    if (componentRef.current) 
-         componentRef.current.themeStyle = themeStyle
+  function animateElement(element: any) {
+    return new Promise(evolve => {
+        element.style.transition = 'color 2s ease';
+        element.style.color = 'red';
+  
+      element?.addEventListener('transitionend', () => {
+        evolve('Animation completed');
+      }, { once: true });
+    });
+  };
+  
+  const handleClick = () =>{
+    const element = document.querySelector('#myElement');
+    animateElement(element)
+      .then(message => {
+        console.log(message);
+      })
+      .catch(error => {
+        console.error('Error during animation:', error);
+      });
+  };
+  
+
+  useEffect(() => {
     removePopup();
-  },[]);
+  }, []);
 
   return (
     <div className="home-page">
-      <CircularGaugeComponent theme={gaugeTheme} ref={componentRef}>
-        <AxesDirective>
-          <AxisDirective>
-            <PointersDirective>
-              <PointerDirective
-                textStyle={fontModel}
-                needleStartWidth={5}
-                value={35}
-              ></PointerDirective>
-            </PointersDirective>
-          </AxisDirective>
-        </AxesDirective>
-      </CircularGaugeComponent>
+      <div id="myElement">
+        My Component
+      </div>
+      <button onClick={handleClick}>Click To Animate</button>
     </div>
   );
 };
